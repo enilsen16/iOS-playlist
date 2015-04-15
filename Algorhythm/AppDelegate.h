7ac6b4a5f6524d77c67a1f0083062fa260f426ae
Erik Nilsen <enilsen16@live.com>
2015-04-15 15:33:28 -0700

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Erik Nilsen on 4/15/15.
+//  Copyright (c) 2015 Erik Nilsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorhythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Erik Nilsen on 4/15/15.
+//  Copyright (c) 2015 Erik Nilsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
