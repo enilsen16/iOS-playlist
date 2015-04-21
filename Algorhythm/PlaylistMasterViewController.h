490258b3a7854afbe48032734bfe73c95c5ed422
Erik Nilsen <enilsen16@live.com>
2015-04-20 17:29:06 -0700

    complete app

--- Algorhythm/PlaylistMasterViewController.h
+++ Algorhythm/PlaylistMasterViewController.h
@@ -10,8 +10,7 @@
 
 @interface PlaylistMasterViewController : UIViewController
 
-
-@property (weak, nonatomic) IBOutlet UIImageView *playlistImageView0;
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *playlistImageViews;
 
 @end
 
